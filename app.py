b74f0c2a24f716c0bebae5b70872d772cab76046
Athisruth <athisruthmsatheesh98@gmail.com>
2024-01-16 10:46:37 +0530

    deployed in flask app from local host

---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask,request,render_template
+import numpy as np
+import pandas as pd
+
+from sklearn.preprocessing import StandardScaler
+from src.pipeline.predict_pipeline import CustomData,PredictPipeline
+
+application=Flask(__name__)
+
+
+app=application
+
+##Route for a home page
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/predictdata',methods=['GET','POST'])
+def predict_datapoint():
+    if request.method=='GET':
+        return render_template('home.html')
+    else:
+        data=CustomData(
+            Hour = request.form.get("Hour"),
+            Temperature = float(request.form.get("Temperature(°C)")),
+            Humidity = float(request.form.get("Humidity(%)")),
+            Wind_speed = float(request.form.get("Wind speed (m/s)")),
+            Visibility = float(request.form.get("Visibility (10m)")),
+            Solar_Radiation = float(request.form.get("Solar Radiation (MJ/m2)")),
+            Rainfall = float(request.form.get("Rainfall(mm)")),
+            Snowfall = float(request.form.get("Snowfall (cm)")),
+            weekday = request.form.get("weekday"),
+            Seasons = request.form.get("Seasons"),
+            Holiday = request.form.get("Holiday"),
+            Functioning_Day = request.form.get("Functioning Day")
+
+        )
+
+        pred_df=data.get_data_as_data_frame()
+        print("pred_df printed",pred_df)
+        predict_pipeline=PredictPipeline()
+        results=predict_pipeline.predict(pred_df)
+        print("results",results)
+        return render_template('home.html',results=int(results[0]))
+    
+if __name__=="__main__":
+    app.run(host="0.0.0.0",debug=True)
+
---
+++ src/pipeline/predict_pipeline.py
@@ -0,0 +1,77 @@
+import sys
+import pandas as pd
+from src.exception import CustomException
+from src.utils import load_object
+
+
+class PredictPipeline:
+    def __init__(self):
+        pass
+    def predict(self,features):
+        try:
+            model_path="artifacts\\model.pkl"
+            preprocessor_path="artifacts\\preprocessor.pkl"
+            model=load_object(file_path=model_path)
+            preprocessor=load_object(file_path=preprocessor_path)
+            print("preprocessor",preprocessor)
+            data_scaled=preprocessor.transform(features)
+            preds=model.predict(data_scaled)
+            print("preds",data_scaled)
+            return preds
+        except Exception as e:
+            raise CustomException(e,sys)
+        
+
+
+class CustomData:
+    def __init__(self,
+                Hour,
+                Temperature,
+                Humidity,
+                Wind_speed,
+                Visibility,
+                Solar_Radiation,
+                Rainfall,
+                Snowfall,
+                weekday,
+                Seasons,
+                Holiday,
+                Functioning_Day
+                                    ):
+        
+        self.Hour = Hour
+        self.Temperature = Temperature
+        self.Humidity = Humidity
+        self.Wind_speed = Wind_speed
+        self.Visibility = Visibility
+        self.Solar_Radiation = Solar_Radiation
+        self.Rainfall = Rainfall
+        self.Snowfall = Snowfall
+        self.weekday = weekday
+        self.Seasons = Seasons
+        self.Holiday = Holiday
+        self.Functioning_Day = Functioning_Day
+
+    def get_data_as_data_frame(self):#The values giving in the webpage will assign to this variables. 
+        try:
+            custom_data_input_dict={
+            'weekday':[self.weekday],
+            'Hour':[self.Hour],
+            'Temperature(°C)':[self.Temperature],
+            'Humidity(%)':[self.Humidity],
+            'Wind speed (m/s)':[self.Wind_speed],
+            'Visibility (10m)':[self.Visibility],
+            'Solar Radiation (MJ/m2)':[self.Solar_Radiation],
+            'Rainfall(mm)':[self.Rainfall],
+            'Snowfall (cm)':[self.Snowfall],
+            'Seasons':[self.Seasons],
+            'Holiday':[self.Holiday],
+            'Functioning Day':[self.Functioning_Day]
+            }
+            print(custom_data_input_dict)
+            print(pd.DataFrame(custom_data_input_dict))
+            return pd.DataFrame(custom_data_input_dict)
+        except Exception as e:
+            raise CustomException(e,sys)
+
+
